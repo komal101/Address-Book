221c5848a97fcead7540c87bbe5a101d229600de
Komal Deru <maverickresearcher07@gmail.com>
2017-02-17 05:04:09 +0000

    Update lib.sh

--- lib.sh
+++ lib.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 K Tandel: 0116-3647821:kd@hotmail.com:7, mwlbourner st., England:M72 274
-Dhanush Shah: 07738375667:dshah@yahoo.co.in:91,daman,vapi,India:334-344
+D Shah: 07738655858375667:dshah@yahoo.co.in:91,daman,India:334-344
